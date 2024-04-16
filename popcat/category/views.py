6d664791f59d2823585ae0841c9b3c12be717056
lv1.turtle <skqltldnjf77@gmail.com>
2024-04-16 18:09:47 +0900

    ERD 수정에 따른 chart/models.py 수정, category/views.py 추가

--- popcat/category/views.py
+++ popcat/category/views.py
@@ -1,4 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from chart.models import *
 
 def index(request):
-    return HttpResponse("category app")
+    topsellers_list = TopSellers.objects.all()
+    category_list = GameCategory.objects.all()
+    
+    context = {'topsellers': topsellers_list, 'categories': category_list}
+    return render(request,'category/index.html',context)
--- popcat/chart/models.py
+++ popcat/chart/models.py
@@ -1,20 +1,20 @@
 from django.db import models
+from datetime import datetime
 
-
-# Create your models here.
 class TopSellers(models.Model):
-    game_name = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True)
+    game_name = models.CharField(max_length=255) # 게임 이름
+    created_at = models.DateTimeField(auto_now_add=True) # DB에 생성된 날짜
 
     def __str__(self):
         return f"게임 이름:{self.game_name}"
 
-
+# fk 수정
 class GameCategory(models.Model):
-    game_name = models.ForeignKey(TopSellers, on_delete=models.CASCADE)
-    reviews = models.IntegerField(default=0)
-    category = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True)
+    topsellers_id = models.ForeignKey(TopSellers, related_name="gamecategories", on_delete=models.CASCADE)
+    game_name = models.CharField(max_length=255) # 게임 이름
+    reviews = models.IntegerField(default=0) # 리뷰어 수 ( 이용자 수와 비례 )
+    category = models.CharField(max_length=255) # 특정 게임에 포함된 여러 카테고리 중 하나
+    created_at = models.DateTimeField(auto_now_add=True) # DB에 생성된 날짜
 
     def __str__(self):
         return f"{self.game_name}"
