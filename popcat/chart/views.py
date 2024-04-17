623d5f70d128b2497dfb5ef633894c551aa93e8c
nooreong2 <nooreong0503@gmail.com>
2024-04-17 13:49:13 +0900

    [UPDATE] models update

--- popcat/chart/views.py
+++ popcat/chart/views.py
@@ -9,17 +9,17 @@ def index(request):
 
 
 ## 오늘 날짜에 해당하는 TopSeller 저장
-def postCrawlData(request):
-    data_Queue = GetReviewCount()
-    for data in data_Queue:
-        # game_name : 신규 -> 신규 저장
-        if not Game.objects.filter(game_name=data[1]).exists():
-            game = Game(game_name=data[1])
-            game.save()
+# def postCrawlData(request):
+#     data_Queue = GetReviewCount()
+#     for data in data_Queue:
+#         # game_name : 신규 -> 신규 저장
+#         if not Game.objects.filter(game_name=data[1]).exists():
+#             game = Game(game_name=data[1])
+#             game.save()
 
-            top_sellers = TopSellers(game_id=game.id)
-            top_sellers.save()
+#             top_sellers = TopSellers(game_id=game.id)
+#             top_sellers.save()
 
-            reviewers = GameReviewers(game_id=game.id, reviewers=data[2])
-            reviewers.save()
-    return HttpResponse(f"{data_Queue}")
+#             reviewers = GameReviewers(game_id=game.id, reviewers=data[2])
+#             reviewers.save()
+#     return HttpResponse(f"{data_Queue}")
