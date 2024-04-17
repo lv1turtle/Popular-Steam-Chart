e7a31c84d0e700235094abda2a4941f749fc0d82
lv1.turtle <skqltldnjf77@gmail.com>
2024-04-17 18:50:39 +0900

    piechart 추가

--- popcat/category/views.py
+++ popcat/category/views.py
@@ -2,16 +2,16 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from chart.models import *
 
+# 테스트 용, 마음대로 수정해주세요.
 def index(request):
     game_list = Game.objects.all()
     topsellers_list = TopSellers.objects.all()
-    category_list = GameCategory.objects.all()
     reviewers_list = GameReviewers.objects.all()
     
     context = {
         'games': game_list,
         'topsellers': topsellers_list,
-        'categories': category_list,
         'reviewers': reviewers_list
     }
+    
     return render(request,'category/index.html',context)
--- popcat/chart/urls.py
+++ popcat/chart/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("api/tag/",views.RankByTagAPIView.as_view(),name="tag_api"),
+    path("tag/", views.TagView.as_view(), name="tag"),
     path("test/", views.postreviewsData, name="get_data"),
 ]
