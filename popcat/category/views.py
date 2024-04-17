db1fc635b95fa8be7702bcbd371e55ed5f857699
poiup <dkswodud0531@gmail.com>
2024-04-17 21:36:04 +0900

    카테고리별 평균값 차트 처리 1차

--- popcat/category/views.py
+++ popcat/category/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from chart.models import *
+from rest_framework.views import APIView
 
 # 테스트 용, 마음대로 수정해주세요.
 def index(request):
@@ -15,3 +16,46 @@ def index(request):
     }
     
     return render(request,'category/index.html',context)
+
+# class average_price_by_categories(APIView):
+
+def average_price_by_categories(request):
+    print(request)
+    category = "salad"
+    # 검색 조건
+    
+    # 검색
+    game_list =  Game.objects.filter(categories__contains=category)[:100]
+    category_Total_price_dict = {}
+    
+    # 카테고리별 가격
+    for i in game_list :
+        categories = [category.strip() for category in i.categories.split(',')]
+        for category in categories :
+            category_Total_price_dict[category] = category_Total_price_dict.get(category, [0,0,0])
+            category_Total_price_dict[category][0] += i.price
+            category_Total_price_dict[category][1] += 1
+    
+    labels = []
+    sizes = []
+    # 카테고리별 평균값 및 차트세팅
+    if category_Total_price_dict :
+        for category in category_Total_price_dict :
+            category_Total_price_dict[category][2] = category_Total_price_dict[category][0] // category_Total_price_dict[category][1]
+            labels.append(category)
+            sizes.append(category_Total_price_dict[category][2])
+    else :
+        labels.append('NONE')
+        sizes.append(1)
+    # 원형 차트 생성
+    
+
+    data = {
+        "games" : game_list,
+        "dict" : category_Total_price_dict,
+        "labels" : labels,
+        "sizes" : sizes,
+        # "chart" : image_png
+    }
+    return render(request, 'category/categoryChart.html', data)
+        
