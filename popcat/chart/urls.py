9b968c065522f3eb603791d878235b9db849cc3a
lv1.turtle <skqltldnjf77@gmail.com>
2024-04-18 21:56:24 +0900

    main page 수정 및 chart 추가

--- popcat/chart/urls.py
+++ popcat/chart/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("api/tag/", views.RankByTagAPIView.as_view(), name="tag_api"),
     path("tag/", views.TagView.as_view(), name="tag"),
+    path("api/barchart/", views.BarChartAPIView.as_view(), name="barchart_api"),
     path("test/", views.postreviewsData, name="get_data"),
     path("main/", views.main, name="main_page"),
 ]
