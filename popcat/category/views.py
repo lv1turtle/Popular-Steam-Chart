3a0f013569930abc7757a6a1e94edaf0d9b6b299
201810773서정연 <64679164+201810773@users.noreply.github.com>
2024-04-18 14:21:45 +0900

    Update views.py

--- popcat/category/views.py
+++ popcat/category/views.py
@@ -29,9 +29,9 @@ def category_search(request):
             'games': game_rank,  # TopSellers에서 가져온 게임들만 표시
             'category': category
         }
-        return render(request, 'category_results.html', context)
+        return render(request, 'category_search.html', context)
     else:
-        return render(request, 'category_results.html', {})
+        return render(request, 'category_search.html', {})
 
 # class average_price_by_categories(APIView):
 
