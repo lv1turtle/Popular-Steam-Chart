7b2c8305ffcc31ce6136a0c596be01ca227aeee9
201810773서정연 <64679164+201810773@users.noreply.github.com>
2024-04-18 14:25:10 +0900

    Update urls.py

--- popcat/category/urls.py
+++ popcat/category/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("categorysearch", views.category_search, name="category_search"),
+    path("categorysearch/", views.category_search, name="category_search"),
     path("test/", views.average_price_by_categories, name="average_price_by_categories"),
 ]
